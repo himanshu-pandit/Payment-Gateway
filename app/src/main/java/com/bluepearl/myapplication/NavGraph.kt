63bbe1f29faddc67870c859dac8dc70ba36953b5
Himanshu Pandit <Lenovo@DESKTOP-L0U0LE4>
2024-12-13 18:52:14 +0530

    remove unwanted code

--- app/src/main/java/com/bluepearl/myapplication/NavGraph.kt
+++ app/src/main/java/com/bluepearl/myapplication/NavGraph.kt
@@ -5,8 +5,6 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.bluepearl.myapplication.screens.IciciPay
-import com.bluepearl.myapplication.screens.PaytmPay
 import com.bluepearl.myapplication.screens.RazorPay
 import com.bluepearl.myapplication.viewmodels.RazorpayVModel
 
@@ -21,14 +19,6 @@ fun NavGraph(
 
         composable(ScreenList.Razorpay.route) {
             RazorPay(innerPadding, razorpayVModel)
-        }
-
-        composable(ScreenList.Icicipay.route){
-            IciciPay(innerPadding)
-        }
-
-        composable(ScreenList.Paytmpay.route){
-            PaytmPay(innerPadding)
         }
 
     }
--- app/src/main/java/com/bluepearl/myapplication/screens/IciciPay.kt
+++
@@ -1,9 +0,0 @@
-package com.bluepearl.myapplication.screens
-
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.runtime.Composable
-
-@Composable
-fun IciciPay(innerPadding: PaddingValues) {
-
-}
